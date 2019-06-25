6769fc361b1473b01e8fcca04ca5b48808415cfa
Paulo Vinicius <Paulovps713312@gmail.com>
2019-06-25 14:53:12 -0300

    Changes to 'leilao' component, added Google Maps API

--- src/app/component/leilao/leilao.component.ts
+++ src/app/component/leilao/leilao.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { Leilao } from './leilao.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
+import { Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-leilao',
   templateUrl: './leilao.component.html',
   styleUrls: ['./leilao.component.css']
 })
+
+@NgModule({
+  imports: [FormsModule, CommonModule],
+  declarations: [LeilaoComponent]
+})
+
 export class LeilaoComponent implements OnInit {
 
-  constructor() { }
+    leilao: Leilao;
+    leilaoRef: AngularFireList<any>;
+    leiloes: any[];
 
-  ngOnInit() {
+    constructor(private db: AngularFireDatabase) { }
+
+    ngOnInit(): void {
+        this.leilao = new Leilao();
+    }
+
+    salvar() {
+      if (this.leilao.key == null) {
+          this.db.list('leiloes').push(this.leilao)
+              .then((result: any) => {
+                  console.log(result.key);
+              });            
+      } else {
+          this.db.list('leiloes').update(this.leilao.key,this.leilao)
+          .then((result: any) => {
+              console.log(result.key);
+          });  
+      }
+  }
+
+  carregar(leilao:Leilao) {
+    this.leilao = new Leilao(leilao.key,
+        leilao.data, leilao.coordenadas);
   }
 
 }
---
+++ src/app/component/leilao/leilao.model.ts
@@ -0,0 +1,13 @@
+export class Leilao {
+
+    constructor(key: string = null, data: string = null, coordenadas: any = null) {
+        this.key = key;
+        this.data = data;
+        this.coordenadas = coordenadas;
+    }
+
+    key: string;
+    data: any;
+    coordenadas: string;
+
+}
